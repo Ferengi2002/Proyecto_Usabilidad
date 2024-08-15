71e9091a6add7dfa89a0abc451d2250ac640484b
Cristofer Paucar <151577243+Cristof10@users.noreply.github.com>
2024-08-15 11:26:39 -0500

    lo que hizo Nathy, preguntenle a ella

--- src/components/ErrorReportScreen.js
+++ src/components/ErrorReportScreen.js
@@ -5,17 +5,17 @@ import ErrorReportForm from './ErrorReportForm';
 
 const ErrorReportScreen = () => {
   return (
-<div className="error-screen" tabindex="0">
-  <div className="header" tabindex="0">
-    <div className="header-container" tabindex="0">
+<div className="error-screen">
+  <div className="header">
+    <div className="header-container">
       <h1 className='abmodel-text' tabindex="0">ABMODEL</h1>
     </div>
   </div>
-  <div className="content" tabindex="0">
-    <div className="suggestions-section" tabindex="0">
+  <div className="content">
+    <div className="suggestions-section">
       <SuggestionsForm tabindex="0" />
     </div>
-    <div className="about-section" tabindex="0">
+    <div className="about-section">
       <p tabindex="0">
         En ABMODEL, fusionamos la innovación con la accesibilidad para dar
         vida a experiencias digitales cautivadoras. Nos especializamos en la
@@ -25,8 +25,8 @@ const ErrorReportScreen = () => {
         creaciones sean inclusivas y fáciles de usar para todos.
       </p>
     </div>
-    <div className="error-section" tabindex="0">
-      <ErrorReportForm tabindex="0" />
+    <div className="error-section">
+      <ErrorReportForm tabindex="0"/>
     </div>
   </div>
 </div>
--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -5,11 +5,11 @@ import Timer from './Timer';
 export default function SubHeader() {
   const { name } = useUserName();
   return (
-    <div className='container' tabindex="0">
+    <div className='container'>
     <div className='timer-container'>
       <h3 tabindex="0">Nombre:</h3>
       <h3 className='text-timer' tabindex="0">{name || 'Nombre'}</h3>
-      <div tabindex="0">
+      <div>
         <Timer tabindex="0"/>
       </div>
     </div>
